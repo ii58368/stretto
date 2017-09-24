be9fc364fdef76da84c724dc970ed72a5b7bbede
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2017-09-24 11:37:12 +0200

    Removed warnings

--- www/dirShift.php
+++ www/dirShift.php
@@ -3,26 +3,26 @@ require 'framework.php';
 
 if ($action == 'update' && $access->auth(AUTH::DIR_RW))
 {
-   $next_status = intval($_REQUEST[stat_dir]) + 1;
+   $next_status = intval(request('stat_dir')) + 1;
    if ($next_status == $db->shi_stat_leave) // deprecated
       $next_status++;
    if ($next_status > 4)
       $next_status = 0;
 
    $query = "update participant set stat_dir = $next_status " .
-           "where id_person = $_REQUEST[id_person] " .
-           "and id_project = $_REQUEST[id_project]";
+           "where id_person = " . request('id_person') . " " .
+           "and id_project = " . request('id_project');
    $db->query($query);
 }
 
-$sel_year = is_null($_REQUEST[from]) ? date("Y") : intval($_REQUEST[from]);
+$sel_year = is_null(request('from')) ? date("Y") : intval(request('from'));
 $prev_year = $sel_year - 1;
 
 echo "
     <h1>Turnus</h1>
     <table border=1>
     <tr>
-      <th bgcolor=#A6CAF0><a href=\"$php_self?from=$prev_year&id_person={$row[person_id]}&id_project={$row[project_id]}&status={$status}&_sort={$sort}\"><img src=images/left.gif border=0 title=\"Forrige &aring;r...\"></a>
+      <th bgcolor=#A6CAF0><a href=\"$php_self?from=$prev_year&_sort=$sort\"><img src=images/left.gif border=0 title=\"Forrige år...\"></a>
          <a href=\"$php_self?from=$sel_year&_sort=firstname,lastname\" title=\"Sorter p&aring; fornavn...\">Fornavn</a>/
          <a href=\"$php_self?from=$sel_year&_sort=lastname,firstname\" title=\"Sorter p&aring; etternavn...\">Etternavn</a></th>
       <th bgcolor=#A6CAF0><a href=\"$php_self?from=$sel_year&_sort=list_order,lastname,firstname\" title=\"Sorter p&aring; instrumentgruppe...\">Instrument</a></th>";
@@ -34,7 +34,7 @@ $query = "SELECT id, name, semester, year " .
 $stmt = $db->query($query);
 
 foreach ($stmt as $row)
-   echo "<th bgcolor=#A6CAF0><a href=dirPlan.php?_sort=time,date&id_project={$row[id]}>{$row[name]}<br>{$row[semester]}{$row[year]}</a></td>";
+   echo "<th bgcolor=#A6CAF0><a href=dirPlan.php?_sort=time,date&id_project=".$row['id'].">".$row['name']."<br>".$row['semester'].$row['year']."</a></td>";
 echo "</tr><tr>";
 
 if (is_null($sort))
@@ -57,29 +57,29 @@ $prev_id = 0;
 
 foreach ($stmt as $row)
 {
-   if ($row[person_id] != $prev_id)
+   if ($row['person_id'] != $prev_id)
    {
-      echo "</tr><tr><td bgcolor=#A6CAF0><a href=myDirection.php?id_person=$row[person_id] title=\"$row[comment]\">$row[firstname] $row[lastname]</a>";
-      if ($row[status_dir] == $db->per_dir_nocarry)
+      echo "</tr><tr><td bgcolor=#A6CAF0><a href=myDirection.php?id_person=".$row['person_id']." title=\"".$row['comment']."\">".$row['firstname']." ".$row['lastname']."</a>";
+      if ($row['status_dir'] == $db->per_dir_nocarry)
          echo " <img src=\"images/chair-minus-icon.png\" border=0 title=\"Kan ikke l&oslash;fte bord\"></h2>";
-      echo "</td><td bgcolor=#A6CAF0> $row[instrument] </td>";
-      $prev_id = $row[person_id];
+      echo "</td><td bgcolor=#A6CAF0>".$row['instrument']."</td>";
+      $prev_id = $row['person_id'];
    }
    $query = "SELECT stat_dir, stat_final, comment_dir from participant " .
-           "where id_project = {$row[project_id]} " .
-           "and id_person = {$row[person_id]} " .
+           "where id_project = ".$row['project_id']." " .
+           "and id_person = ".$row['person_id']." " .
            "and (stat_final = $db->par_stat_yes or not stat_dir = $db->shi_stat_free)";
    $stmt2 = $db->query($query);
    echo "<td align=center>";
    foreach ($stmt2 as $row2)
    {
-      $status = $row2[stat_dir];
-      if ($row2[stat_dir] != $db->shi_stat_free && $row2[stat_final] != $db->par_stat_yes)
+      $status = $row2['stat_dir'];
+      if ($row2['stat_dir'] != $db->shi_stat_free && $row2['stat_final'] != $db->par_stat_yes)
          $status = $db->shi_stat_dropout;
-      $comment = $row2[comment_dir];
-      $img = "<img src=\"images/shift_status_{$status}.gif\" border=0 title=\"{$db->shi_stat[$status]} ({$row[project_name]}) {$comment}\">";
+      $comment = $row2['comment_dir'];
+      $img = "<img src=\"images/shift_status_{$status}.gif\" border=0 title=\"{$db->shi_stat[$status]} (".$row['project_name'].") {$comment}\">";
       if ($access->auth(AUTH::DIR_RW))
-         echo "<a href=\"$_SERVER[PHP_SELF]?_action=update&id_person={$row[person_id]}&id_project={$row[project_id]}&stat_dir={$status}&from=$sel_year&_sort={$sort}\">$img</a>\n";
+         echo "<a href=\"".$_SERVER['PHP_SELF']."?_action=update&id_person=".$row['person_id']."&id_project=".$row['project_id']."&stat_dir={$status}&from=$sel_year&_sort={$sort}\">$img</a>\n";
       else
          if ($status != $db->shi_stat_free)
             echo $img;
