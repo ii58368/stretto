18c8b728612253902d3a04ca28ace46cad2274c4
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2018-04-18 16:56:13 +0200

    Updated GUI for leave

--- www/leave.php
+++ www/leave.php
@@ -5,7 +5,9 @@ if (is_null($sort))
    $sort = 'ts_reg';
 
 echo "
-<h1>Permisjoner (langtid)</h1>";
+<h1>Permisjoner (langtid)</h1>
+Her registreres permisjoner som strekker seg over en lenger tidsperiode, typisk et helt semester eller mer.<br>
+Permisjoner for enkeltprosjekter registrers under Admin->Ressurser-><i>prosjektnavn</i><p>";
 if ($access->auth(AUTH::LEAVE_RW))
    echo "
     <form action=\"$php_self\" method=post>
@@ -22,9 +24,9 @@ if ($access->auth(AUTH::LEAVE_RW))
       <th>Edit</th>";
 echo "
       <th><a href=\"$php_self?_sort=ts_reg\"title=\"Sorter på registreringsdato...\">Registrert</a></th>
+      <th>Endret</th>
       <th>Navn</th>
       <th><a href=\"$php_self?_sort=status,ts_reg\"title=\"Sorter på søknadsstatus, deretter registreringsdato...\">Status</a></th>
-      <th>Endret</th>
       <th>Fra</th>
       <th>Til</th>
       <th>Tekst</th>
@@ -80,12 +82,12 @@ if ($action == 'new')
     <td align=left><input type=hidden name=_action value=update>
     <input type=hidden name=_sort value=\"$sort\">
     <input type=submit value=ok title=\"Lagre\"></td>
-    <td>" . strftime('%a %e.%b %y') . "</td>\n<td>";
+    <td>" . strftime('%e.%b %y') . "</td>
+    <td>" . strftime('%e.%b %y') . "</td>\n<td>";
    select_person(null);
    echo "</td>\n<td>";
    select_status(null);
    echo "</td>
-    <td>" . strftime('%a %e.%b %y') . "</td>
     <td><input type=date size=10 name=ts_from title=\"Dato permisjonssøknaden gjelder fra. Format: eks: 3. dec 2017\"></td>
     <td><input type=date size=10 name=ts_to title=\"Dato permisjonssøknaden gjelder til. Format: eks: 3. dec 2017\"></td>
     <td><textarea name=text wrap=virtual cols=60 rows=10 title=\"Fritekst\"></textarea></td>
@@ -177,12 +179,12 @@ foreach ($stmt as $row)
            <a href=\"$php_self?_sort=$sort&_action=view&_no=" . $row['id'] . "&ts_reg=" . $season->year() . "\"><img src=\"images/cross_re.gif\" border=0 title=\"Klikk for å editere...\"></a>
              </center></td>";
       echo
-      "<td>" . strftime('%a %e.%b %y', $row['ts_reg']) . "</td>" .
+      "<td>" . strftime('%e.%b %y', $row['ts_reg']) . "</td>" .
+      "<td>" . strftime('%e.%b %y', $row['ts_proc']) . "</td>" .
       "<td>" . $row['firstname'] . " " . $row['middlename'] . " " . $row['lastname'] . " (" . $row['instrument'] . ")</td>" .
       "<td>" . $db->lea_stat[$row['status']] . "</td>" .
-      "<td>" . strftime('%a %e.%b %y', $row['ts_proc']) . "</td>" .
-      "<td>" . strftime('%a %e.%b %y', $row['ts_from']) . "</td>" .
-      "<td>" . strftime('%a %e.%b %y', $row['ts_to']) . "</td>" .
+      "<td>" . strftime('%e.%b %y', $row['ts_from']) . "</td>" .
+      "<td>" . strftime('%e.%b %y', $row['ts_to']) . "</td>" .
       "<td>";
       echo str_replace("\n", "<br>\n", $row['text']);
       echo "</td>" .
@@ -196,13 +198,13 @@ foreach ($stmt as $row)
     <td nowrap><input type=submit value=ok title=Lagre>
       <input type=submit value=del name=_delete onClick=\"return confirm('Sikkert at du vil slette " . date('D j.M y', $row['ts_reg']) . "?');\" title=\"Slett...\"></td>
     <td>" . strftime('%e.%m.%y', $row['ts_reg']) . "</td>
+    <td>" . strftime('%e.%m.%y') . "</td>
     <td>";
       select_person($row['id_person']);
       echo "</td>
     <td>";
       select_status($row['status']);
       echo "</td>
-    <td>" . strftime('%e.%m.%y') . "</td>
     <td><input type=date size=10 name=ts_from value=\"" . date('Y-m-d', $row['ts_from']) . "\" title=\"Dato permisjonssøknaden gjelder fra. Format: eks: 3. dec 2017\"></td>
     <td><input type=date size=10 name=ts_to value=\"" . date('Y-m-d', $row['ts_to']) . "\" title=\"Dato permisjonssøknaden gjelder til. Format: eks: 3. dec 2017\"></td>
      <td><textarea cols=60 rows=10 wrap=virtual name=text title=Fritekst>" . $row['text'] . "</textarea></td>
