4e106040500972547c0aeabd498c69a57e1cd919
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2018-04-23 06:55:52 +0200

    Added automatic hyperlinks for web references

--- www/event.php
+++ www/event.php
@@ -3,6 +3,23 @@ require 'framework.php';
 
 $id_project = request('id_project');
 
+function replace_links( $text ) 
+{    
+	$text = preg_replace('#(script|about|applet|activex|chrome):#is', "\\1:", $text);
+	$ret = ' ' . $text;
+	
+	// Replace Links with http://
+	$ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\" rel=\"nofollow\">\\2</a>", $ret);
+	
+	// Replace Links without http://
+	$ret = preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\" rel=\"nofollow\">\\2</a>", $ret);
+	// Replace Email Addresses
+	$ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
+	$ret = substr($ret, 1);
+	
+	return $ret;
+}
+
 function select_project($selected)
 {
    global $db;
@@ -214,6 +231,7 @@ foreach ($stmt as $row)
      </tr>
      </table>";
       $body = str_replace("\n", "<br>\n", $row['body']);
+      $body = replace_links($body);
       echo ($row['status'] == $db->evt_status_draft) ? "<font color=grey>$body</font>" : $body;
    }
    echo "<p>";
