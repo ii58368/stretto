76bfa3a40c678454c35f3946b8f8c43a66ca9ffc
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2021-11-12 07:16:46 +0100

    Omit headings if section contains no data

--- www/prjinfo_pdf.php
+++ www/prjinfo_pdf.php
@@ -19,13 +19,13 @@ class PDF extends PDF_util
       $stmt = $db->query($query);
       $prj = $stmt->fetch(PDO::FETCH_ASSOC);
 
-      $this->header1($prj['name']." ".$prj['semester']."-".$prj['year']);
+      $this->header1($prj['name'] . " " . $prj['semester'] . "-" . $prj['year']);
       $txt = str_replace("<ul>", "", $prj['info']);
       $txt = str_replace("<li>", "- ", $txt);
       $txt = str_replace("</ul>", "", $txt);
       $txt = str_replace("<u>", "", $txt);
       $txt = str_replace("</u>", "", $txt);
-      
+
       $this->MultiCell(0, 5, $this->sconv($txt));
       $this->Ln();
    }
@@ -39,48 +39,41 @@ class PDF extends PDF_util
               . " from repository, music"
               . " where repository.id = music.id_repository"
               . " and music.status = $db->mus_stat_yes"
-              . " and music.id_project = ".request('id')." "
+              . " and music.id_project = " . request('id') . " "
               . " order by lastname, firstname, work";
 
       $stmt = $db->query($query);
 
-      $this->header1("Repertoar");
-      $col1 = 40;
-
-      foreach ($stmt as $row)
+      if ($stmt->rowCount() > 0)
       {
-         $this->Cell($col1, 5, $this->sconv($row['firstname']." ".$row['lastname'].":"));
-         $this->Cell(80, 5, $this->sconv($row['title']));
-         if (strlen($row['work']) > 0)
-            $this->Cell(60, 5, $this->sconv("fra ".$row['work']));
+         $this->header1("Repertoar");
+         $col1 = 40;
+
+         foreach ($stmt as $row)
+         {
+            $this->Cell($col1, 5, $this->sconv($row['firstname'] . " " . $row['lastname'] . ":"));
+            $this->Cell(80, 5, $this->sconv($row['title']));
+            if (strlen($row['work']) > 0)
+               $this->Cell(60, 5, $this->sconv("fra " . $row['work']));
+            $this->Ln();
+            if (strlen($row['r_comment']) > 0)
+            {
+               $this->Cell($col1, 5);
+               $this->MultiCell(100, 5, trim($this->sconv($row['r_comment'])));
+            }
+            if (strlen($row['comment']) > 0)
+            {
+               $this->Cell($col1, 5);
+               $this->MultiCell(100, 5, trim($this->sconv($row['comment'])));
+            }
+         }
          $this->Ln();
-         if (strlen($row['r_comment']) > 0)
-         {
-            $this->Cell($col1, 5);
-            $this->MultiCell(100, 5, trim($this->sconv($row['r_comment'])));
-         }
-         if (strlen($row['comment']) > 0)
-         {
-            $this->Cell($col1, 5);
-            $this->MultiCell(100, 5, trim($this->sconv($row['comment'])));
-         }
       }
-      $this->Ln();
    }
 
    function plan($id_project)
    {
       global $db;
-   
-      $this->header1("Prøveplan");
-
-      $w = array(30, 30, 30, 100);
-      $h = array("Dato", "Prøvetid", "Lokale", "Merknad");
-
-      $this->setFillColor(0xE0, 0xE0, 0xE0);
-      for ($i = 0; $i < count($w); $i++)
-         $this->Cell($w[$i], 5, $this->sconv($h[$i]), 0, 0, "L", true);
-      $this->Ln();
 
       $query = "SELECT date, time, " .
               "plan.location as location, location.name as lname, " .
@@ -95,16 +88,29 @@ class PDF extends PDF_util
 
       $stmt = $db->query($query);
 
-      $height = 4;
-      foreach ($stmt as $row)
+      if ($stmt->rowCount() > 0)
       {
-         $this->Cell($w[0], $height, $this->sconv(strftime('%a %e.%b', $row['date'])));
-         $this->Cell($w[1], $height, $row['time']);
-         $this->Cell($w[2], $height, $this->sconv($row['lname']." ".$row['location']));
-         $this->MultiCell($w[3], $height, $this->sconv($row['comment']));
-         $this->Ln(1);
+         $this->header1("Prøveplan");
+
+         $w = array(30, 30, 30, 100);
+         $h = array("Dato", "Prøvetid", "Lokale", "Merknad");
+
+         $this->setFillColor(0xE0, 0xE0, 0xE0);
+         for ($i = 0; $i < count($w); $i++)
+            $this->Cell($w[$i], 5, $this->sconv($h[$i]), 0, 0, "L", true);
+         $this->Ln();
+
+         $height = 4;
+         foreach ($stmt as $row)
+         {
+            $this->Cell($w[0], $height, $this->sconv(strftime('%a %e.%b', $row['date'])));
+            $this->Cell($w[1], $height, $row['time']);
+            $this->Cell($w[2], $height, $this->sconv($row['lname'] . " " . $row['location']));
+            $this->MultiCell($w[3], $height, $this->sconv($row['comment']));
+            $this->Ln(1);
+         }
+         $this->Ln();
       }
-      $this->Ln();
    }
 
    function participants($id_project)
@@ -123,28 +129,31 @@ class PDF extends PDF_util
 
       $stmt = $db->query($query);
 
-      $this->header1("Musikere");
-      $this->Ln(2);
-      
-      $this->colStart();
-      $last_instrument = '';
-      
-      foreach ($stmt as $e)
+      if ($stmt->rowCount() > 0)
       {
-         if ($last_instrument != $e['instrument'])
+         $this->header1("Musikere");
+         $this->Ln(2);
+
+         $this->colStart();
+         $last_instrument = '';
+
+         foreach ($stmt as $e)
          {
-            $this->colLn();            
+            if ($last_instrument != $e['instrument'])
+            {
+               $this->colLn();
+               if ($this->GetY() > $this->GetPageHeight() - 30)
+                  $this->colNext(41);
+               $this->bold($e['instrument'] . ":");
+               $this->colLn(2);
+            }
+            $name = $e['firstname'] . " " . $e['lastname'];
+            $this->Cell(0, 4, $this->sconv($name));
+            $this->colLn();
             if ($this->GetY() > $this->GetPageHeight() - 30)
                $this->colNext(41);
-            $this->bold($e['instrument'].":");
-            $this->colLn(2);
+            $last_instrument = $e['instrument'];
          }
-         $name = $e['firstname']." ".$e['lastname'];
-         $this->Cell(0, 4, $this->sconv($name));
-         $this->colLn();
-         if ($this->GetY() > $this->GetPageHeight() - 30)
-            $this->colNext(41);
-         $last_instrument = $e['instrument'];
       }
    }
 
