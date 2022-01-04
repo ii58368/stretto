0b43af8da9bf85abbff81d272fdc5882120c4285
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2022-01-04 07:21:09 +0100

    Refact with class TABLE()

--- www/dirProject.php
+++ www/dirProject.php
@@ -19,21 +19,25 @@ function list_group($id)
 
    $s = $db->query($q);
 
+   $str = '';
+   
    foreach ($s as $e)
    {
       if ($e['status'] == $db->shi_stat_tentative)
-         echo "<font color=grey>";
+         $str .= "<font color=grey>";
       if ($e['status'] == $db->shi_stat_failed)
-         echo "<strike>";
-      echo $e['firstname'] . " " . $e['lastname'] . " (" . $e['instrument'] . ")";
+         $str .= "<strike>";
+      $str .= $e['firstname'] . " " . $e['lastname'] . " (" . $e['instrument'] . ")";
       if ($e['status'] == $db->shi_stat_failed)
-         echo "</strike>";
+         $str .= "</strike>";
       if ($e['status'] == $db->shi_stat_tentative)
-         echo "</font>";
+         $str .= "</font>";
       if ($e['status_dir'] == $db->per_dir_nocarry)
-         echo "<image src=images/chair-minus-icon.png border=0 title=\"Kan ikke løfte bord\">";
-      echo "<br>";
+         $str .= "<image src=images/chair-minus-icon.png border=0 title=\"Kan ikke løfte bord\">";
+      $str .= "<br>";
    }
+   
+   return $str;
 }
 
 function select_person($selected)
@@ -46,13 +50,18 @@ function select_person($selected)
 
    $s = $db->query($q);
 
+   $str = "<select name=id_person>\n";
+   
    foreach ($s as $e)
    {
-      echo "<option value=\"" . $e['id'] . "\"";
+      $str .= "<option value=\"" . $e['id'] . "\"";
       if ($e['id'] == $selected)
-         echo " selected";
-      echo ">" . $e['firstname'] . " " . $e['lastname'] . " (" . $e['instrument'] . ")";
+         $str .= " selected";
+      $str .= ">" . $e['firstname'] . " " . $e['lastname'] . " (" . $e['instrument'] . ")";
    }
+   $str .= "</select>\n";
+   
+   return $str;
 }
 
 function mail2dir($id_project)
@@ -71,10 +80,10 @@ function mail2dir($id_project)
    $s = $db->query($q);
    $r = $s->fetchAll(PDO::FETCH_ASSOC);
 
-   echo "<a href=\"mailto:";
+   $str = "<a href=\"mailto:";
    foreach ($r as $e)
       if (strlen($e['email']) > 0)
-         echo $e['email'] . ",";
+         $str .= $e['email'] . ",";
 
    $q = "select email, phone1 "
            . "from person, project "
@@ -84,40 +93,39 @@ function mail2dir($id_project)
    $r2 = $s2->fetch(PDO::FETCH_ASSOC);
 
    if (strlen($r2['email']) > 0)
-      echo $r2['email'];
+      $str .= $r2['email'];
 
-   echo "?subject=OSO: Regikomit&eacute;, $project_name&body=Se oppdatert regiplan: http://" . $_SERVER['SERVER_NAME'] . "/stretto/direction.php?id_project=$id_project\"><image border=0 src=images/image1.gif hspace=20 title=\"Send mail alle i regikomit&eacute;en\"></a>";
+   $str .= "?subject=OSO: Regikomit&eacute;, $project_name&body=Se oppdatert regiplan: http://" . $_SERVER['SERVER_NAME'] . "/stretto/direction.php?id_project=$id_project\"><image border=0 src=images/image1.gif hspace=20 title=\"Send mail alle i regikomit&eacute;en\"></a>";
 
-   echo "<a href=\"sms:";
+   $str .= "<a href=\"sms:";
    reset($r);
-   $str = '';
+   $s = '';
    foreach ($r as $e)
       if (strlen($e['phone1']) > 0)
-         $str .= $e['phone1'] . ",";
+         $s .= $e['phone1'] . ",";
    if (strlen($r2['phone1']) > 0)
-      $str .= $r2['phone1'] . ",";
-   $str = str_replace(' ', '', $str);
-   echo substr($str, 0, -1);
-   echo "&body=OSO Regikomit&eacute:\"><image border=0 src=images/sms.png hspace=20 title=\"Send SMS til alle i regikomit&eacute;en\"></a>";
+      $s .= $r2['phone1'] . ",";
+   $str .= str_replace(' ', '', $s);
+   $str = substr($str, 0, -1);
+   $str .= "&body=OSO Regikomit&eacute:\"><image border=0 src=images/sms.png hspace=20 title=\"Send SMS til alle i regikomit&eacute;en\"></a>";
+   
+   return $str;
 }
 
-echo "
-    <h1>Regiprosjekt</h1>
-    <form action='$php_self' method=post>
-    <table border=1>
-    <tr>";
-if ($access->auth(AUTH::DIR_RW))
-   echo "
-      <th>Edit</th>";
-echo "
-      <th><a href=\"$php_self?_sort=name,id\" title=\"Sorter p&aring; prosjektnavn\">Prosjekt</a></th>
-      <th nowrap><a href=\"$php_self?_sort=year,semester+DESC,id\" title=\"Sorter p&aring; semester\">Sem</a></th>
-      <th>Status</th>
-      <th>Regiansvarlig</th>
-      <th>Regikomit&eacute;</th>
-      <th>Generell info</th>
-    </tr>";
+echo "<h1>Regiprosjekt</h1>";
 
+$form = new FORM();
+$tb = new TABLE();
+
+if ($access->auth(AUTH::DIR_RW))
+   $tb->th("Edit");
+$tb->th("<a href=\"$php_self?_sort=name,id\" title=\"Sorter p&aring; prosjektnavn\">Prosjekt</a>");
+$tb->th("<a href=\"$php_self?_sort=year,semester+DESC,id\" title=\"Sorter p&aring; semester\">Sem</a>", 'nowrap');
+$tb->th("Status");
+$tb->th("Type");
+$tb->th("Regiansvarlig");
+$tb->th("Regikomit&eacute;");
+$tb->th("Generell info");
 
 if ($action == 'update' && $access->auth(AUTH::DIR_RW))
 {
@@ -139,63 +147,47 @@ if ($action == 'update' && $access->auth(AUTH::DIR_RW))
 }
 
 $query = "SELECT project.id as id, name, semester, year, id_person, project.status as status, " .
-        "firstname, lastname, instrument, info_dir " .
+        "firstname, lastname, instrument, info_dir, orchestration " .
         "FROM person, project, instruments " .
         "where project.id_person = person.id " .
         "and id_instruments = instruments.id " .
         "and project.year = " . $season->year() . " " .
         "and project.semester = '" . $season->semester() . "' " .
-        "and not project.status = " . $db->prj_stat_internal . " " .
+        "and not project.orchestration = " . $db->prj_type_social . " " .
         "order by " . str_replace("+", " ", $sort);
 
 $stmt = $db->query($query);
 
 foreach ($stmt as $row)
 {
+   $tb->tr();
+
    if ($row['id'] != $no)
    {
-      echo "<tr>";
       if ($access->auth(AUTH::DIR_RW))
-         echo "
-        <td><center>
-            <a href=\"$php_self?_sort=$sort&_action=view&_no=" . $row['id'] . "\"><img src=\"images/cross_re.gif\" border=0 title=\"Klikk for &aring; editere...\"></a>
-             </center></td>";
-      echo
-      "<td><a href=\"dirPlan.php?id_project=" . $row['id'] . "\">" . $row['name'] . "</a></td>" .
-      "<td>" . $row['semester'] . " " .
-      "    " . $row['year'] . "</td>" .
-      "<td>" . $db->prj_stat[$row['status']] . "</td>" .
-      "<td>" . $row['firstname'] . " " . $row['lastname'] . " (" . $row['instrument'] . ")</td>" .
-      "<td nowrap>";
-      mail2dir($row['id']);
-      echo "<br>";
-      list_group($row['id']);
-      echo "</td><td>";
-      echo str_replace("\n", "<br>\n", $row['info_dir']);
-      echo "</td>" .
-      "</tr>";
-   } else
+         $tb->td("<a href=\"$php_self?_sort=$sort&_action=view&_no=" . $row['id'] . "\"><img src=\"images/cross_re.gif\" border=0 title=\"Klikk for &aring; editere...\"></a>", 'align=center');
+      $tb->td("<a href=\"dirPlan.php?id_project=" . $row['id'] . "\">" . $row['name'] . "</a>");
+      $tb->td($row['semester'] . " " . " " . $row['year']);
+      $tb->td($db->prj_stat[$row['status']]);
+      $tb->td($db->prj_type[$row['orchestration']]);
+      $tb->td($row['firstname'] . " " . $row['lastname'] . " (" . $row['instrument'] . ")", 'nowrap');
+      $tb->td(mail2dir($row['id']) . "<br>" . list_group($row['id']));
+      $tb->td(str_replace("\n", "<br>\n", $row['info_dir']));      
+   } 
+   else
    {
-      echo "<tr>
-    <input type=hidden name=_action value=update>
+      $tb->td("<input type=hidden name=_action value=update>
     <input type=hidden name=_sort value='$sort'>
     <input type=hidden name=_no value='$no'>
     <input type hidden name=id_person value=" . $row['id_person'] . ">
-    <th nowrap><input type=submit value=ok title=\"Lagere endring\" >
-    <th>" . $row['name'] . "</th>
-    <th>" . $row['semester'] . " " . $row['year'] . "</th>
-    <th>" . $db->prj_stat[$row['status']] . "</th>
-    <th><select name=id_person>";
-      select_person($row['id_person']);
-      echo "</select></th>";
-      echo "<td>";
-      list_group($row['id']);
-      echo " </td>
-    <th><textarea cols=44 rows=10 wrap=virtual name=info_dir>" . $row['info_dir'] . "</textarea></th>
-    </tr>";
+    <input type=submit value=ok title=\"Lagere endring\" >", 'nowrap');
+      
+    $tb->td($row['name']);
+    $tb->td($row['semester'] . " " . $row['year']);
+    $tb->td($db->prj_stat[$row['status']]);
+    $tb->td($db->prj_type[$row['orchestration']]);
+    $tb->td(select_person($row['id_person']));
+    $tb->td(list_group($row['id']));
+    $tb->td("<textarea cols=44 rows=10 wrap=virtual name=info_dir>" . $row['info_dir'] . "</textarea>");
    }
 }
-?> 
-
-</table>
-</form>
