201d7d6b42799ab11980637be728b6bd1c81fd22
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2022-09-14 08:20:20 +0200

    Added redirect to https server if requesting old http server

--- www/index.php
+++ www/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if ($_SERVER['SERVER_NAME'] == "internal.oslosymfoniorkester.no")
+{
+   echo "<html><head><meta http-equiv=\"Refresh\" content=\"0; url=https://stretto.oslosymfoniorkester.no\" /></head></html>";
+   exit(0);
+}
+
 require_once 'auth.php';
 
 if (count($_GET) == 0)
