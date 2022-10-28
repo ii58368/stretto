ca7b56315506a29333e7fe62f9452b44c647fcc1
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2022-10-28 17:04:48 +0200

    refactured to make use of class table()

--- www/plan.php
+++ www/plan.php
@@ -6,34 +6,38 @@ $id_project = is_null(request('id_project')) ? "%" : request('id_project');
 
 function select_tsort($selected)
 {
-   echo "<select name=tsort title=\"Sorteringsrekkefølge dersom dette en av flere aktiviteter på samme dato\">";
+   $str = "<select name=tsort title=\"Sorteringsrekkefølge dersom dette en av flere aktiviteter på samme dato\">";
    for ($i = 0; $i < 8; $i++)
    {
-      echo "<option value=$i";
+      $str .= "<option value=$i";
       if ($i == $selected)
-         echo " selected";
-      echo ">" . $i;
+         $str .= " selected";
+      $str .= ">" . $i;
    }
-   echo "</select>";
+   $str .= "</select>";
+   
+   return $str;
 }
 
 function select_location($selected)
 {
    global $db;
 
-   echo "<select name=id_location title=\"Velg prøvested fra listen over registrerte lokaler\">";
+   $str = "<select name=id_location title=\"Velg prøvested fra listen over registrerte lokaler\">";
 
    $q = "SELECT id, name FROM location order by name";
    $s = $db->query($q);
 
    foreach ($s as $e)
    {
-      echo "<option value=\"" . $e['id'] . "\"";
+      $str .= "<option value=\"" . $e['id'] . "\"";
       if ($e['id'] == $selected)
-         echo " selected";
-      echo ">" . $e['name'];
+         $str .= " selected";
+      $str .= ">" . $e['name'];
    }
-   echo "</select>";
+   $str .= "</select>";
+   
+   return $str;
 }
 
 function select_project($selected)
@@ -41,7 +45,7 @@ function select_project($selected)
    global $db;
    global $season;
 
-   echo "<select name=sid_project title=\"Velg hvilket prosjekt prøven gjelder for...\">";
+   $str = "<select name=sid_project title=\"Velg hvilket prosjekt prøven gjelder for...\">";
 
    $year = date("Y");
    $q = "SELECT id, name, semester, year, orchestration FROM project " .
@@ -52,14 +56,16 @@ function select_project($selected)
 
    foreach ($s as $e)
    {
-      echo "<option value=\"" . $e['id'] . "\"";
+      $str .= "<option value=\"" . $e['id'] . "\"";
       if ($e['id'] == $selected)
-         echo " selected";
-      echo ">" . $e['name'] . " (" . $e['semester'], $e['year'] . ")";
+         $str .= " selected";
+      $str .= ">" . $e['name'] . " (" . $e['semester'] . $e['year'] . ")";
       if ($e['orchestration'] == $db->prj_type_reduced)
-         echo '*';
+         $str .= '*';
    }
-   echo "</select>";
+   $str .= "</select>";
+   
+   return $str;
 }
 
 echo "<h1>Spilleplan</h1>\n";
@@ -81,42 +87,31 @@ if ($access->auth(AUTH::PLAN_RW))
    echo "<a href=\"$php_self?id_project=$id_project&_action=new&date=$ndate&id_location=" . request('id_location') . "\" title=\"Registrer ny prøve...\"><img src=\"images/new_inc.gif\" border=0 hspace=5 vspace=5></a>\n";
 echo "<a href=\"plan_pdf.php\" title=\"PDF versjon...\"><img src=images/pdf.jpeg height=22 border=0 hspace=5 vspace=5></a>\n";
 
-echo "
-    <form action='$php_self' method=post>
-    <table border=1>
-    <tr>";
+$form = new FORM();
+$tb = new TABLE('border=1');
+
 if ($access->auth(AUTH::PLAN_RW))
-   echo "
-      <th>Edit</th>";
-echo "
-      <th>Dato</th>
-      <th>Prøvetid</th>
-      <th>Lokale</th>
-      <th>Prosjekt</th>
-      <th>Merknad</th>
-    </tr>";
+   $tb->th("Edit");
+
+$tb->th("Dato");
+$tb->th("Prøvetid");
+$tb->th("Lokale");
+$tb->th("Prosjekt");
+$tb->th("Merknad");
+$tb->tr();
 
 $hlp_date = "Format: <dato>. <mnd> [<år>] Merk: Måned på engelsk. Eksempel: 12. dec";
 
 if ($action == 'new')
 {
-   echo "<tr>
-    <td align=left><input type=hidden name=_action value=update>
-    <input type=submit value=ok title=\"Registrer...\"></td>
-    <td><input type=date size=10 name=date value=$ndate title=\"$hlp_date\"></td>
-    <td nowrap>";
-   select_tsort(null);
-   echo "<input type=text size=11 name=time value=\"18:30-21:30\" title=\"Prøvetid\"></td>
-    <td>";
-   select_location(request('id_location'));
-   echo "<br><input type=text size=22 name=location title=\"Prøvested som ikke finnes på listen som typisk bare skal benyttes en gang\">";
-   echo "</td>
-    <td>";
-   select_project($id_project);
-   echo "
-  </td>
-  <td><textarea cols=50 rows=6 wrap=virtual name=comment title=\"Fritekst\">Tutti</textarea></td>
-  </tr>";
+   $tb->td("<input type=hidden name=_action value=update>
+    <input type=submit value=ok title=\"Registrer...\">", 'align=left');
+   $tb->td("<input type=date size=10 name=date value=$ndate title=\"$hlp_date\">" . select_tsort(null), 'nowrap');
+   $tb->td("<input type=text size=11 name=time value=\"18:30-21:30\" title=\"Prøvetid\">");
+   $tb->td(select_location(request('id_location')) . "<br><input type=text size=22 name=location title=\"Prøvested som ikke finnes på listen som typisk bare skal benyttes en gang\">");
+   $tb->td(select_project($id_project));
+   $tb->td("<textarea cols=50 rows=6 wrap=virtual name=comment title=\"Fritekst\">Tutti</textarea>");
+   $tb->tr();
 }
 
 if ($action == 'update' && $access->auth(AUTH::PLAN_RW))
@@ -188,6 +183,8 @@ $last_time = '';
 
 foreach ($stmt as $row)
 {
+   $tb->tr();
+   
    if ($row['id'] != $no || $action != 'view')
    {
       $gfont = '<font>';
@@ -197,54 +194,31 @@ foreach ($stmt as $row)
          $gfont = "<font color=lightgrey>";
 
       if ($access->auth(AUTH::PLAN_RW))
-         echo "<tr>
-        <td><center>
-            <a href=\"$php_self?_action=view&_no=".$row['id']."&id_project=$id_project\"><img src=\"images/cross_re.gif\" border=0 title=\"Klikk for å editere...\"></a>
-             </center></td>";
-      echo "<td>$gfont";
-      if ($access->auth(AUTH::PLAN_RW) || $row['date'] != $last_date)
-        echo strftime('%a %e.%b %y', $row['date']);
-      echo "</font></td><td>$gfont";
-      if ($access->auth(AUTH::PLAN_RW) || $row['date'] != $last_date || $row['time'] != $last_time)
-         echo $row['time'];
-      echo "</font></td><td>$gfont";
-      if (strlen($row['url']) > 0)
-         echo "<a href=\"".$row['url']."\">".$row['lname']."</a>";
-      else
-         echo $row['lname'];
-      echo " ".$row['location'];
-      echo "</font></td><td>$gfont<a href=\"prjInfo.php?id=".$row['id_project']."\">".$row['pname']."</a>";
-      if ($row['orchestration'] == $db->prj_type_reduced)
-         echo '*';
-      echo "</font></td><td>$gfont";
-      echo str_replace("\n", "<br>\n", $row['comment']);
-      echo "</font></td>" .
-      "</tr>";
+         $tb->td ("<a href=\"$php_self?_action=view&_no=".$row['id']."&id_project=$id_project\"><img src=\"images/cross_re.gif\" border=0 title=\"Klikk for å editere...\"></a>", 'align=center');      
+      $date = ($access->auth(AUTH::PLAN_RW) || $row['date'] != $last_date) ? strftime('%a %e.%b %y', $row['date']) : "";
+      $tb->td($gfont . $date . "</font>");
+      $time = ($access->auth(AUTH::PLAN_RW) || $row['date'] != $last_date || $row['time'] != $last_time) ? $row['time'] : "";
+      $tb->td($gfont . $time . "</font>");
+      $url = $access->hlink(strlen($row['url']) > 0, $row['url'], $row['lname']);
+      $tb->td($gfont . $url . " " . $row['location'] . "</font>");
+      $mark = $row['orchestration'] == $db->prj_type_reduced ? '*' : '';
+      $tb->td($access->hlink2("prjInfo.php?id=".$row['id_project'], $row['pname'] . $mark));
+      $tb->td($gfont . str_replace("\n", "<br>\n", $row['comment']) . "</fonnt>");
    }
    else
    {
-      echo "<tr>
+      $tb->td("
     <input type=hidden name=_action value=update>
     <input type=hidden name=_no value='$no'>
-    <td nowrap><input type=submit value=ok>
-    <input type=submit value=del name=_delete onClick=\"return confirm('Sikkert at du vil slette" . strftime('%e.%m.%y', $row['date']) . "?');\"></td>
-    <td><input type=date size=10 name=date value=\"" . date('Y-m-d', $row['date']) . "\" title=\"$hlp_date\"></td>
-    <td nowrap>";
-      select_tsort($row['tsort']);
-      echo "<input type=text size=11 name=time value=\"".$row['time']."\" title=\"Prøvetid\"></td>
-    <td>";
-      select_location($row['id_location']);
-      echo "<br><input type=text size=22 name=location value=\"".$row['location']."\" title=\"Prøvested som ikke finnes på listen som typisk bare skal benyttes en gang\">";
-      echo "</td>
-    <td>";
-      select_project($row['id_project']);
-      echo "</td>
-    <td><textarea cols=50 rows=6 wrap=virtual name=comment title=\"Fritekst\">".$row['comment']."</textarea></td>
-    </tr>";
+    <input type=submit value=ok>
+    <input type=submit value=del name=_delete onClick=\"return confirm('Sikkert at du vil slette" . strftime('%e.%m.%y', $row['date']) . "?');\">", 'nowrap');
+      $tb->td("<input type=date size=10 name=date value=\"" . date('Y-m-d', $row['date']) . "\" title=\"$hlp_date\">" . select_tsort($row['tsort']), 'nowrap');
+      $tb->td("<input type=text size=11 name=time value=\"".$row['time']."\" title=\"Prøvetid\">");
+      $tb->td(select_location($row['id_location'])
+        . "<br><input type=text size=22 name=location value=\"".$row['location']."\" title=\"Prøvested som ikke finnes på listen som typisk bare skal benyttes en gang\">");
+      $tb->td(select_project($row['id_project']));
+      $tb->td("<textarea cols=50 rows=6 wrap=virtual name=comment title=\"Fritekst\">".$row['comment']."</textarea>");
    }
    $last_date = $row['date'];
    $last_time = $row['time'];
 }
-
-echo "</table>
-</form>";
