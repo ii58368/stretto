8f6ce2f3aebaa73d967ffc0159c7a3f99f47422d
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2023-02-02 17:11:08 +0100

    index page redirects directly to myplan if refered from login page

--- www/index.php
+++ www/index.php
@@ -1,18 +1,18 @@
 <?php
 
-if ($_SERVER['SERVER_NAME'] == "internal.oslosymfoniorkester.no")
-{
-   echo "<html><head><meta http-equiv=\"Refresh\" content=\"0; url=https://stretto.oslosymfoniorkester.no\" /></head></html>";
-   exit(0);
-}
-
 require_once 'auth.php';
 
 if (count($_GET) == 0)
    $season->reset();
 
 
-
-include 'myplan.php';
+if (empty($_SERVER['HTTP_REFERER']))
+{
+   include 'welcome.php';
+}
+else
+{
+   include 'myplan.php';
+}
 
 
