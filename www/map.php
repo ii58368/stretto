f22641b86e0093233b84c239bebfbb6f0447c181
Jan Olav Rolfsnes <jan.olav.rolfsnes@gmail.com>
2024-01-08 07:27:00 +0100

    Increased seating image

--- www/map.php
+++ www/map.php
@@ -229,7 +229,7 @@ $template = array(
     $v1_20
 );
 
-$img = imagecreatetruecolor(650, 650);
+$img = imagecreatetruecolor(650, 690);
 $bgcol = imagecolorAllocate($img, 0xff, 0xfc, 0xf5);
 imagefill($img, 0, 0, $bgcol);
 
